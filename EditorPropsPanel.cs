43648c9bf78aaddf49d8f0362551f7258bffed09
Egor Aralov <earalov@thumbtack.net>
2015-08-24 15:57:46 +0600

    Prepare release

--- EditorPropsPanel.cs
+++ EditorPropsPanel.cs
@@ -7,60 +7,74 @@ using ColossalFramework.DataBinding;
 using ColossalFramework.UI;
 using UnityEngine;
 
-namespace Terraforming {
-	public class EditorPropsPanel : GeneratedScrollPanel {
-		private static MethodInfo m_CreateAssetItem = ReflectionUtils.GetInstanceMethod (typeof (GeneratedScrollPanel), "CreateAssetItem");
+namespace MoreBeautification
+{
+    public class EditorPropsPanel : GeneratedScrollPanel
+    {
+        private static MethodInfo m_CreateAssetItem = ReflectionUtils.GetInstanceMethod(typeof(GeneratedScrollPanel), "CreateAssetItem");
 
-		public string[] m_editorCategories;
+        public string[] m_editorCategories;
 
-		public void CreateAssetItem (PrefabInfo info) {
-			ReflectionUtils.InvokeInstanceMethod (m_CreateAssetItem, this, info);
-		}
+        public void CreateAssetItem(PrefabInfo info)
+        {
+            ReflectionUtils.InvokeInstanceMethod(m_CreateAssetItem, this, info);
+        }
 
-		public override ItemClass.Service service {
-			get {
-				return ItemClass.Service.None;
-			}
-		}
+        public override ItemClass.Service service
+        {
+            get
+            {
+                return ItemClass.Service.None;
+            }
+        }
 
-		protected override void OnButtonClicked (UIComponent comp) {
-			object objectUserData = comp.objectUserData;
-			PropInfo propInfo = objectUserData as PropInfo;
-			if (propInfo != null)
-			{
-				PropTool propTool = ToolsModifierControl.SetTool<PropTool> ();
-				if (propTool != null)
-				{
-					propTool.m_prefab = propInfo;
-				}
-			}
-		}
+        protected override void OnButtonClicked(UIComponent comp)
+        {
+            object objectUserData = comp.objectUserData;
+            PropInfo propInfo = objectUserData as PropInfo;
+            if (propInfo != null)
+            {
+                PropTool propTool = ToolsModifierControl.SetTool<PropTool>();
+                if (propTool != null)
+                {
+                    propTool.m_prefab = propInfo;
+                }
+            }
+        }
 
-		public override void RefreshPanel () {
-			base.RefreshPanel ();
+        public override void RefreshPanel()
+        {
+            base.RefreshPanel();
 
-			List<PrefabInfo> list = new List<PrefabInfo> ();
-			foreach (PrefabInfo info in Resources.FindObjectsOfTypeAll<PrefabInfo> ()) {
-				if (Array.Exists (this.m_editorCategories, c => c == info.editorCategory)) {
-					list.Add (info);
-				}
-			}
-			list.Sort (new Comparison<PrefabInfo> (this.ItemsGenericCategorySort));
+            List<PrefabInfo> list = new List<PrefabInfo>();
+            foreach (PrefabInfo info in Resources.FindObjectsOfTypeAll<PrefabInfo>())
+            {
+                if (Array.Exists(this.m_editorCategories, c => c == info.editorCategory))
+                {
+                    list.Add(info);
+                }
+            }
+            list.Sort(new Comparison<PrefabInfo>(this.ItemsGenericCategorySort));
 
-			foreach (PrefabInfo info in list) {
-				this.CreateAssetItem (info);
-			}
-		}
+            foreach (PrefabInfo info in list)
+            {
+                this.CreateAssetItem(info);
+            }
+        }
 
-		protected int ItemsGenericCategorySort (PrefabInfo a, PrefabInfo b) {
-			if (a.editorCategory == b.editorCategory) {
-				return a.m_UIPriority.CompareTo (b.m_UIPriority);
-			} else {
-				int aID = Array.FindIndex (this.m_editorCategories, c => c == a.editorCategory);
-				int bID = Array.FindIndex (this.m_editorCategories, c => c == b.editorCategory);
-				return aID - bID;
-			}
-		}
-	}
+        protected int ItemsGenericCategorySort(PrefabInfo a, PrefabInfo b)
+        {
+            if (a.editorCategory == b.editorCategory)
+            {
+                return a.m_UIPriority.CompareTo(b.m_UIPriority);
+            }
+            else
+            {
+                int aID = Array.FindIndex(this.m_editorCategories, c => c == a.editorCategory);
+                int bID = Array.FindIndex(this.m_editorCategories, c => c == b.editorCategory);
+                return aID - bID;
+            }
+        }
+    }
 }
 
--- ReflectionUtils.cs
+++ ReflectionUtils.cs
@@ -1,31 +1,36 @@
 using System;
 using System.Reflection;
 
-namespace Terraforming {
-	public static class ReflectionUtils {
-		/// <summary>
-		/// Uses reflection to get the field value from an object.
-		/// https://stackoverflow.com/a/3303182/1011428
-		/// </summary>
-		///
-		/// <param name="type">The instance type.</param>
-		/// <param name="instance">The instance object.</param>
-		/// <param name="fieldName">The field's name which is to be fetched.</param>
-		///
-		/// <returns>The field value from the object.</returns>
-		public static FieldInfo GetInstanceField (Type type, string fieldName) {
-			BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
-			return type.GetField (fieldName, bindFlags);
-		}
+namespace MoreBeautification
+{
+    public static class ReflectionUtils
+    {
+        /// <summary>
+        /// Uses reflection to get the field value from an object.
+        /// https://stackoverflow.com/a/3303182/1011428
+        /// </summary>
+        ///
+        /// <param name="type">The instance type.</param>
+        /// <param name="instance">The instance object.</param>
+        /// <param name="fieldName">The field's name which is to be fetched.</param>
+        ///
+        /// <returns>The field value from the object.</returns>
+        public static FieldInfo GetInstanceField(Type type, string fieldName)
+        {
+            BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+            return type.GetField(fieldName, bindFlags);
+        }
 
-		public static MethodInfo GetInstanceMethod (Type type, string methodName) {
-			BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
-			return type.GetMethod (methodName, bindFlags);
-		}
+        public static MethodInfo GetInstanceMethod(Type type, string methodName)
+        {
+            BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+            return type.GetMethod(methodName, bindFlags);
+        }
 
-		public static object InvokeInstanceMethod (MethodInfo method, object instance, params object[] args) {
-			return method.Invoke (instance, args);
-		}
-	}
+        public static object InvokeInstanceMethod(MethodInfo method, object instance, params object[] args)
+        {
+            return method.Invoke(instance, args);
+        }
+    }
 }
 
